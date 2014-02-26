882ea5cb05e8fe77a9a24b430a6a802dc6bfd664
zz <zhuguang@ejoy.com>
2014-02-26 11:44:03 +0800

    [ADDED] viewlist排序

--- editor_src2/easycomplex/src/libcomplex/StagePanel.h
+++ editor_src2/easycomplex/src/libcomplex/StagePanel.h
@@ -25,6 +25,7 @@ namespace complex
 		//
 		virtual void removeSprite(d2d::ISprite* sprite);
 		virtual void insertSprite(d2d::ISprite* sprite);
+		virtual void resetSpriteOrder(d2d::ISprite* sprite, bool up);
 
 		Symbol* getSymbol() { return m_symbol; }
 
