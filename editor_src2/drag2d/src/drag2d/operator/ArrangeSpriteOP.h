7ba5999c64465b9ec3542f909f437db7799c26d2
zz <zhuguang@ejoy.com>
2014-02-26 21:35:33 +0800

    [FIXED] 去掉透明边后捕捉的问题

--- editor_src2/drag2d/src/drag2d/operator/ArrangeSpriteOP.h
+++ editor_src2/drag2d/src/drag2d/operator/ArrangeSpriteOP.h
@@ -9,140 +9,142 @@
 
 namespace d2d
 {
-	class EditPanel;
-	class MultiSpritesImpl;
-	class PropertySettingPanel;
-	class AbstractEditCMPT;
 
-	template <typename TBase>
-	class ArrangeSpriteOP : public TBase
+class EditPanel;
+class MultiSpritesImpl;
+class PropertySettingPanel;
+class AbstractEditCMPT;
+
+template <typename TBase>
+class ArrangeSpriteOP : public TBase
+{
+public:
+	ArrangeSpriteOP(EditPanel* editPanel, MultiSpritesImpl* spritesImpl, 
+		PropertySettingPanel* propertyPanel = NULL, AbstractEditCMPT* callback = NULL,
+		bool isDeformOpen = true);
+	virtual ~ArrangeSpriteOP();
+
+	virtual bool onKeyDown(int keyCode);
+	virtual bool onKeyUp(int keyCode);
+	virtual bool onMouseLeftDown(int x, int y);
+	virtual bool onMouseLeftUp(int x, int y);
+	virtual bool onMouseRightDown(int x, int y);
+	virtual bool onMouseRightUp(int x, int y);
+	virtual bool onMouseDrag(int x, int y);
+
+	virtual bool onPopMenuSelected(int type);
+
+	virtual bool onDraw() const;
+	virtual bool clear();
+
+protected:
+	virtual void translateSprite(const Vector& delta);
+	virtual void rotateSprite(const Vector& dst);
+	virtual void scaleSprite(const Vector& currPos);
+	virtual void shearSprite(const Vector& currPos);
+
+	virtual void setRightPopupMenu(wxMenu& menu);
+	virtual bool isOffsetEnable() const { return true; };
+
+private:
+	void setScalingFromSelected();
+
+	void autoAlign(const std::vector<ISprite*>& sprites);
+	void autoAlign(const ISprite* src, ISprite* dst);
+
+	// 0 - 3 are scale, 4 - 7 are shear
+	// 0 4 1
+	// 5   6
+	// 2 7 3
+	static void getSpriteCtrlNodes(const ISprite* sprite, Vector nodes[8]);
+
+private:
+	class TranslateVisitor : public IVisitor
 	{
 	public:
-		ArrangeSpriteOP(EditPanel* editPanel, MultiSpritesImpl* spritesImpl, 
-			PropertySettingPanel* propertyPanel = NULL, AbstractEditCMPT* callback = NULL,
-			bool isDeformOpen = true);
-		virtual ~ArrangeSpriteOP();
-
-		virtual bool onKeyDown(int keyCode);
-		virtual bool onKeyUp(int keyCode);
-		virtual bool onMouseLeftDown(int x, int y);
-		virtual bool onMouseLeftUp(int x, int y);
-		virtual bool onMouseRightDown(int x, int y);
-		virtual bool onMouseRightUp(int x, int y);
-		virtual bool onMouseDrag(int x, int y);
-
-		virtual bool onPopMenuSelected(int type);
-
-		virtual bool onDraw() const;
-		virtual bool clear();
-
-	protected:
-		virtual void translateSprite(const Vector& delta);
-		virtual void rotateSprite(const Vector& dst);
-		virtual void scaleSprite(const Vector& currPos);
-		virtual void shearSprite(const Vector& currPos);
-
-		virtual void setRightPopupMenu(wxMenu& menu);
-		virtual bool isOffsetEnable() const { return true; };
+		TranslateVisitor(const Vector& delta);
+		virtual void visit(Object* object, bool& bFetchNext);
 
 	private:
-		void setScalingFromSelected();
+		const Vector& m_delta;
 
-		void autoAlign(const std::vector<ISprite*>& sprites);
-		void autoAlign(const ISprite* src, ISprite* dst);
+	}; // TranslateVisitor
 
-		// 0 - 3 are scale, 4 - 7 are shear
-		// 0 4 1
-		// 5   6
-		// 2 7 3
-		static void getSpriteCtrlNodes(const ISprite* sprite, Vector nodes[8]);
-
-	private:
-		class TranslateVisitor : public IVisitor
-		{
-		public:
-			TranslateVisitor(const Vector& delta);
-			virtual void visit(Object* object, bool& bFetchNext);
-
-		private:
-			const Vector& m_delta;
-
-		}; // TranslateVisitor
-
-		class RotateVisitor : public IVisitor
-		{
-		public:
-			RotateVisitor(const Vector& start, const Vector& end);
-			virtual void visit(Object* object, bool& bFetchNext);
-
-		private:
-			const Vector &m_start, &m_end;
-
-		}; // RotateVisitor
+	class RotateVisitor : public IVisitor
+	{
+	public:
+		RotateVisitor(const Vector& start, const Vector& end);
+		virtual void visit(Object* object, bool& bFetchNext);
 
 	private:
-		static const float SCALE_NODE_RADIUS;
+		const Vector &m_start, &m_end;
 
-	private:
-		PropertySettingPanel* m_propertyPanel;
+	}; // RotateVisitor
 
-	protected:
-		MultiSpritesImpl* m_spritesImpl;
+private:
+	static const float SCALE_NODE_RADIUS;
 
-		SpriteSelection* m_selection;
-		Vector m_lastPos;
+private:
+	PropertySettingPanel* m_propertyPanel;
 
-		ISprite* m_selected;
+protected:
+	MultiSpritesImpl* m_spritesImpl;
 
-		Vector m_firstPos;
+	SpriteSelection* m_selection;
+	Vector m_lastPos;
 
-	private:
-		// 0 4 1
-		// 5   6
-		// 2 7 3
-		enum CtrlNodeType
-		{
-			LEFT_UP = 0,
-			RIGHT_UP,
-			LEFT_DOWN,
-			RIGHT_DOWN,
-			UP,
-			LEFT,
-			RIGHT,
-			DOWN
-		};
+	ISprite* m_selected;
 
-		struct CtrlNode
-		{
-			Vector pos;
-			CtrlNodeType type;
+	Vector m_firstPos;
 
-			void setInvalid() {
-				pos.setInvalid();
-			}
-			bool isValid() {
-				return pos.isValid();
-			}
-		};
-		
-	private:
-		bool m_isDeformOpen;
+private:
+	// 0 4 1
+	// 5   6
+	// 2 7 3
+	enum CtrlNodeType
+	{
+		LEFT_UP = 0,
+		RIGHT_UP,
+		LEFT_DOWN,
+		RIGHT_DOWN,
+		UP,
+		LEFT,
+		RIGHT,
+		DOWN
+	};
 
-		bool m_bRightPress;
+	struct CtrlNode
+	{
+		Vector pos;
+		CtrlNodeType type;
 
-		CtrlNode m_ctrlNodeSelected;
-		bool m_selOffset;
+		void setInvalid() {
+			pos.setInvalid();
+		}
+		bool isValid() {
+			return pos.isValid();
+		}
+	};
+	
+private:
+	bool m_isDeformOpen;
 
-		bool m_bDirty;
+	bool m_bRightPress;
 
-		bool m_autoAlignOpen;
-		Vector m_autoAlignHor[2], m_autoAlignVer[2];
+	CtrlNode m_ctrlNodeSelected;
+	bool m_selOffset;
 
-		ShapeStyle m_shearNodeStyle;
+	bool m_bDirty;
 
-		friend class SpritePropertySetting;
+	bool m_autoAlignOpen;
+	Vector m_autoAlignHor[2], m_autoAlignVer[2];
 
-	}; // ArrangeSpriteOP
+	ShapeStyle m_shearNodeStyle;
+
+	friend class SpritePropertySetting;
+
+}; // ArrangeSpriteOP
+
 }
 
 #include "ArrangeSpriteOP.cpp"
