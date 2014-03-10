4ce490a0a91c15db90b8820b66eb444196aadf40
zz <zhuguang@ejoy.com>
2014-03-10 23:42:03 +0800

    [ADDED] style type

--- editor_src2/drag2d/src/drag2d/common/color_config.h
+++ editor_src2/drag2d/src/drag2d/common/color_config.h
@@ -7,9 +7,17 @@ namespace d2d
 {
 
 static Colorf LIGHT_GREY(0.8f, 0.8f, 0.8f, 0.8f);
-static Colorf RED(1, 0, 0);
-static Colorf LIGHT_RED(0.8f, 0, 0, 0.8f);
+
+static Colorf MID_RED(0.8f, 0, 0, 0.8f);
+static Colorf LIGHT_RED(0.22f, 0, 0, 0.5f);
+
+static Colorf LIGHT_GREEN(0, 0.22f, 0, 0.5f);
+
+static Colorf LIGHT_BLUE(0, 0, 0.22f, 0.5f);
+
 static Colorf BLACK(0, 0, 0, 1);
+
+static Colorf SELECT_RED(0.75f, 0, 0, 1);
 
 }
 
