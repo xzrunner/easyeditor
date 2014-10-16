7fb09ec21bb453c3511bb9132b5d1201e3764aa5
zz <zhuguang@ejoy.com>
2014-10-16 11:19:33 +0800

    [ADDED] shapeshader

---
+++ easysketch/src/easy3d/ShapeShader.h
@@ -0,0 +1,16 @@
+#ifndef _E3D_SHAPE_SHADER_H_
+#define _E3D_SHAPE_SHADER_H_
+
+#include <drag2d.h>
+
+namespace e3d
+{
+
+class ShapeShader : public d2d::IShader
+{
+
+}; // ShapeShader
+
+}
+
+#endif // _E3D_SHAPE_SHADER_H_
