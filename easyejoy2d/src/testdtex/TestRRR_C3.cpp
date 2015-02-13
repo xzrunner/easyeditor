9613f4e17d990432b6ff554fa7343389bf2027a0
zz <zhuguang@ejoy.com>
2015-02-13 18:00:22 +0800

    finish rrr test
    Former-commit-id: dd793bd1baa6f4081344f3cb3dacb5d06cddceac

--- easyejoy2d/src/testdtex/TestRRR_C3.cpp
+++ easyejoy2d/src/testdtex/TestRRR_C3.cpp
@@ -24,35 +24,28 @@ TestRRR_C3::~TestRRR_C3()
 
 void TestRRR_C3::Load()
 {	
-	// C3
 	ej_package* pkg = dtexf_c3_load_pkg("2013baji", "test-dtex/2013baji.epd", 1);
-//	dtexf_c3_load_pkg("2013baji", "test-dtex/2013baji.epp", 1);
-//	dtexf_c3_load_pkg_finish();
+	dtexf_c3_load_pkg("2013baji", "test-dtex/2013baji.rrr", 1);
+	dtexf_c3_load_pkg_finish();
 
-	// RRR
-	dtexf_load_pkg("2013baji", "test-dtex/2013baji.rrr");
-
-	// 
-//	dtex_rrr_load_texture();
-
-//	m_spr = new eejoy2d::EJSprite(pkg, "2013baji1_cheer_1");
+	m_spr = new eejoy2d::EJSprite(pkg, "2013baji1_attack1_1");
 }
 
 void TestRRR_C3::Update()
 {
-//	m_spr->Update();
+	m_spr->Update();
 }
 
 void TestRRR_C3::Draw() const
 {
-// 	eejoy2d::EJScreen* scr = eejoy2d::EJScreen::Instance();
-// 	assert(scr);
-// 	scr->Bind();
-// 	scr->Clear();
-// 
-// 	m_spr->Draw(300, 300);
-// 
-// 	scr->UnBind();
+	eejoy2d::EJScreen* scr = eejoy2d::EJScreen::Instance();
+	assert(scr);
+	scr->Bind();
+	scr->Clear();
+
+	m_spr->Draw(300, 300);
+
+	scr->UnBind();
 }
 
 }
