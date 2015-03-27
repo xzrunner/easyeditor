e5df85d8525eb552c3fdd7a6e981b8c8e87ef77e
zz <zhuguang@ejoy.com>
2015-03-27 19:23:17 +0800

    [CHANGED] libsketch用assimp导入数据
    Former-commit-id: 7802a6172316f73f267345a5accfbc3a64de0aaf

--- easysketch/src/libsketch/LibraryPage.cpp
+++ easysketch/src/libsketch/LibraryPage.cpp
@@ -17,49 +17,49 @@ bool LibraryPage::isHandleSymbol(d2d::ISymbol* symbol) const
 	return dynamic_cast<Symbol*>(symbol) != NULL;
 }
 
+// void LibraryPage::onAddPress(wxCommandEvent& event)
+// {
+// 	wxString filter = wxString(wxT("*_")) + FILE_TAG + wxT(".json");
+// 	wxFileDialog dlg(this, wxT("choose obj files"), wxEmptyString, 
+// 		wxEmptyString, filter, wxFD_OPEN | wxFD_MULTIPLE);
+// 	if (dlg.ShowModal() == wxID_OK)
+// 	{
+// 		wxArrayString filenames;
+// 		dlg.GetPaths(filenames);
+// 		for (size_t i = 0, n = filenames.size(); i < n; ++i)
+// 		{
+//  			d2d::ISymbol* symbol = d2d::SymbolMgr::Instance()->fetchSymbol(filenames[i]);
+//  			m_list->insert(symbol);
+//  			symbol->Release();
+// 		}
+// 	}
+// }
+
+// for test assimp loader
 void LibraryPage::onAddPress(wxCommandEvent& event)
 {
-	wxString filter = wxString(wxT("*_")) + FILE_TAG + wxT(".json");
-	wxFileDialog dlg(this, wxT("choose obj files"), wxEmptyString, 
-		wxEmptyString, filter, wxFD_OPEN | wxFD_MULTIPLE);
+	wxFileDialog dlg(this, wxT("choose model files"));
 	if (dlg.ShowModal() == wxID_OK)
 	{
 		wxArrayString filenames;
 		dlg.GetPaths(filenames);
 		for (size_t i = 0, n = filenames.size(); i < n; ++i)
 		{
- 			d2d::ISymbol* symbol = d2d::SymbolMgr::Instance()->fetchSymbol(filenames[i]);
- 			m_list->insert(symbol);
- 			symbol->Release();
+			e3d::ModelParametric* model = new e3d::ModelParametric();
+			e3d::AssimpHelper loader;
+			e3d::AABB aabb;
+			loader.LoadFile(filenames[i], *model, aabb);
+			libsketch::Symbol* symbol = new libsketch::Symbol();
+			symbol->SetModel(model);
+
+			wxString filepath = FILE_TAG;
+			filepath += ".json";
+			symbol->SetFilepath(filepath);
+			symbol->SetAABB(aabb);
+
+			AddItem(symbol);
 		}
 	}
 }
-
-//// for test assimp loader
-//void LibraryPage::onAddPress(wxCommandEvent& event)
-//{
-//	wxFileDialog dlg(this, wxT("choose model files"));
-//	if (dlg.ShowModal() == wxID_OK)
-//	{
-//		wxArrayString filenames;
-//		dlg.GetPaths(filenames);
-//		for (size_t i = 0, n = filenames.size(); i < n; ++i)
-//		{
-//			e3d::ModelParametric* model = new e3d::ModelParametric();
-//			e3d::AssimpHelper loader;
-//			e3d::AABB aabb;
-//			loader.LoadFile(filenames[i], *model, aabb);
-//			libsketch::Symbol* symbol = new libsketch::Symbol();
-//			symbol->SetModel(model);
-//
-//			wxString filepath = FILE_TAG;
-//			filepath += ".json";
-//			symbol->SetFilepath(filepath);
-//			symbol->SetAABB(aabb);
-//
-//			AddItem(symbol);
-//		}
-//	}
-//}
 
 }
