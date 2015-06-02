28f8bb3ba72536492ee1b18aaac0213bf89c5cc9
zz <zhuguang@ejoy.com>
2015-06-02 14:30:23 +0800

    [FIXED] 升降机别
    Former-commit-id: b10cd6ab67d45ec2a6b1c2ecbed6ed3d2d80e2ee

--- sg/src/sg/StagePanel.cpp
+++ sg/src/sg/StagePanel.cpp
@@ -171,43 +171,6 @@ void StagePanel::SetPerspective(bool is_flat)
  	m_is_flat = is_flat; 
 }
 
-//void StagePanel::changeSpritesLevel(bool up)
-//{
-//	std::vector<d2d::ISprite*> sprites;
-//	getSpriteSelection()->Traverse(d2d::FetchAllVisitor<d2d::ISprite>(sprites));
-//	for (int i = 0, n = sprites.size(); i < n; ++i)
-//	{
-//		d2d::ISprite* sprite = sprites[i];
-//		wxString filepath = sprite->getSymbol().getFilepath();
-//		if (!filepath.Contains("lv")) {
-//			continue;
-//		}
-//
-//		int s = filepath.find("lv") + 2;
-//		int e = filepath.find('_', s-1);
-//		if (s == -1 || e == -1) {
-//			continue;
-//		}
-//
-//		wxString sLevel = filepath.substr(s, e - s);
-//		double level;
-//		sLevel.ToDouble(&level);
-//		level = up ? level + 1 : level - 1;
-//
-//		wxString newfilepath = filepath.substr(0, s) + wxString::FromDouble(level) + filepath.substr(e);
-//
-//		if (!wxFileName::FileExists(newfilepath)) {
-//			continue;
-//		}
-//
-//		d2d::ISymbol* symbol = d2d::SymbolMgr::Instance()->fetchSymbol(newfilepath);
-//		if (symbol) {
-//			sprite->setSymbol(symbol);
-//		}
-//		symbol->Release();
-//	}
-//}
-
 void StagePanel::ChangeSelectedSpritesLevel(bool up)
 {
 	std::vector<d2d::ISprite*> sprites;
@@ -216,39 +179,55 @@ void StagePanel::ChangeSelectedSpritesLevel(bool up)
 	{
 		d2d::ISprite* sprite = sprites[i];
 
-		SymbolExt* info = static_cast<SymbolExt*>(sprite->getSymbol().getUserData());
-		if (info == NULL || info->building->levels.size() == 1) {
+		SpriteExt* spr_info = static_cast<SpriteExt*>(sprite->getUserData());
+		SymbolExt* symbol_info = static_cast<SymbolExt*>(sprite->getSymbol().getUserData());
+		assert(spr_info && symbol_info);
+
+		if (spr_info->level == 1 && !up ||
+			spr_info->level == symbol_info->building->levels.size() && up) {
 			continue;
 		}
 
-		const BuildingCfg::Item* pItem;
-		if (up && info->level != info->building->levels.size() - 1) {
-			pItem = &info->building->levels[info->level + 1];
-		} else if (!up && info->level != 0) {
-			pItem = &info->building->levels[info->level - 1];
-		} else {
-			continue;
+		spr_info->level = up ? spr_info->level + 1 : spr_info->level - 1;
+		std::string new_filepath = symbol_info->building->levels[spr_info->level - 1].res_snapshoot_path;
+		d2d::ISymbol* new_symbol = d2d::SymbolMgr::Instance()->fetchSymbol(new_filepath);
+		if (new_symbol != &sprite->getSymbol()) {
+			SymbolExt* new_symbol_info = static_cast<SymbolExt*>(new_symbol->getUserData());
+			new_symbol_info->remain--;
+			symbol_info->remain++;
+			sprite->setSymbol(new_symbol);
 		}
 
-		if (pItem->town_hall_level > m_base_level) {
-			return;
-		}
-
-		d2d::ISymbol* symbol = d2d::SymbolMgr::Instance()->fetchSymbol(pItem->res_snapshoot_path);
-		if (symbol) {
-			if (symbol->getUserData() == NULL) 
-			{
-				SymbolExt* new_info = new SymbolExt;
-				new_info->size = info->size;
-				new_info->remain = info->remain;
-				new_info->wall_type = info->wall_type;
-				new_info->level = pItem->level;
-				new_info->building = pItem->building;
-				symbol->setUserData(new_info);
-			}
-			sprite->setSymbol(symbol);
-		}
-		symbol->Release();
+// 		//////////////////////////////////////////////////////////////////////////
+// 
+// 		const BuildingCfg::Item* pItem;
+// 		if (up && symbol_info->level != symbol_info->building->levels.size() - 1) {
+// 			pItem = &symbol_info->building->levels[symbol_info->level + 1];
+// 		} else if (!up && symbol_info->level != 0) {
+// 			pItem = &symbol_info->building->levels[symbol_info->level - 1];
+// 		} else {
+// 			continue;
+// 		}
+// 
+// 		if (pItem->town_hall_level > m_base_level) {
+// 			return;
+// 		}
+// 
+// 		d2d::ISymbol* symbol = d2d::SymbolMgr::Instance()->fetchSymbol(pItem->res_snapshoot_path);
+// 		if (symbol) {
+// 			if (symbol->getUserData() == NULL) 
+// 			{
+// 				SymbolExt* new_info = new SymbolExt;
+// 				new_info->size = symbol_info->size;
+// 				new_info->remain = symbol_info->remain;
+// 				new_info->wall_type = symbol_info->wall_type;
+// 				new_info->level = pItem->level;
+// 				new_info->building = pItem->building;
+// 				symbol->setUserData(new_info);
+// 			}
+// 			sprite->setSymbol(symbol);
+// 		}
+// 		symbol->Release();
 	}
 }
 
