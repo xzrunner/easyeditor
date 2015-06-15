e3e63b4801768e06a0b7e3e9a5856a4e3152576f
zz <zhuguang@ejoy.com>
2015-06-15 17:37:09 +0800

    [ADDED] 更透明的浅灰色
    Former-commit-id: 0923836af62f36028a91fd57e2c2a4268c5bb48a

--- drag2d/src/drag2d/common/color_config.h
+++ drag2d/src/drag2d/common/color_config.h
@@ -9,6 +9,7 @@ namespace d2d
 static Colorf NULL_COLOR(0, 0, 0, 0);
 
 static Colorf LIGHT_GREY(0.8f, 0.8f, 0.8f, 0.8f);
+static Colorf LIGHT_GREY_ALPHA(0.8f, 0.8f, 0.8f, 0.4f);
 
 static Colorf MID_RED(0.8f, 0, 0, 0.8f);
 static Colorf LIGHT_RED(0.22f, 0, 0, 0.5f);
