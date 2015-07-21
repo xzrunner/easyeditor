8c4bba8b15500e0d10ca256e0bf43be82d367e6f
zz <zhuguang@ejoy.com>
2015-07-21 19:01:11 +0800

    [CHANGED] test async data
    Former-commit-id: 4360303620027c2ba1cc01c727e909615677ed66

--- easyejoy2d/src/testdtex/TestAsync.cpp
+++ easyejoy2d/src/testdtex/TestAsync.cpp
@@ -27,31 +27,31 @@ TestAsync::~TestAsync()
 
 void TestAsync::Load()
 {
-	LoadSprite("2001dashi", "2001dashi1_build2_3", 0.25f);
-// 	LoadSprite("2002jumin", "2002jumin1_run_3", 0.25f);
-// 	LoadSprite("2003daoke", "2003daoke5_attack2_2", 0.25f);
-// 	LoadSprite("2004shenshe", "2004shenshe5_attack1_2", 0.25f);
-// 	LoadSprite("2005mojin", "2005mojin3_run_1", 0.25f);
-// 	LoadSprite("2006jiabing", "2006jiabing5_attack1_3", 0.25f);
-// 	LoadSprite("2007sishi", "2007sishi5_attack1_3", 0.25f);
-// 	LoadSprite("2008tianzai", "2008tianzai6_attack1_3", 0.25f);
-// 	LoadSprite("2009daoshi", "2009daoshi3_run_3", 0.25f);
-// 	LoadSprite("2010shennv", "2010shennv4_attack1_2", 0.25f);
-// 	LoadSprite("2011dapeng", "2011dapeng1_circlestyle_attack_1", 0.25f);
-// 	LoadSprite("2012fujia", "2012fujia3_bomb_1", 0.25f);
-// 	LoadSprite("2013baji", "2013baji1_attack2_3", 0.25f);
-// 	LoadSprite("2014yingyan", "2014yingyan1_run_3", 0.25f);
-// 	LoadSprite("2022xueying", "xueying_ghosttype_attack_1", 0.25f);
-// 	LoadSprite("2201yini", "2201yini1_attack1_1", 0.25f);
-// 	LoadSprite("2202xiyu", "2202xiyu1_run_3", 0.25f);
-// 	LoadSprite("2203langqi", "2203langqi2_cheer_1", 0.25f);
-// 	LoadSprite("2204wuzhu", "2204wuzhu1_cheer1_1", 0.25f);
-// 	LoadSprite("2205huoyuansu", "2205huoyuansu_summon_1", 0.25f);
-// 	LoadSprite("2206jijian", "2206jijian1_attack5_1", 0.25f);
-// 	LoadSprite("2208tieta", "2208tieta2_attack1_1", 0.25f);
-// 	LoadSprite("2209cunminnv", "cunminnv_run_1", 0.25f);
-// 	LoadSprite("2210cunminnan", "cunminnan_run_1", 0.25f);
-// 	LoadSprite("2212huojuren", "2212huojuren_attack1_3", 0.25f);	
+//  	LoadSprite("2001dashi", "2001dashi1_run_1", 0.25f);
+//  	LoadSprite("2002jumin", "2002jumin1_run_3", 0.25f);
+//   	LoadSprite("2003daoke", "2003daoke5_attack2_2", 0.25f);
+//   	LoadSprite("2004shenshe", "2004shenshe5_attack1_2", 0.25f);
+//  	LoadSprite("2005mojin", "2005mojin3_run_1", 0.25f);
+//  	LoadSprite("2006jiabing", "2006jiabing5_attack1_3", 0.25f);
+//  	LoadSprite("2007sishi", "2007sishi5_attack1_3", 0.25f);
+//   	LoadSprite("2008tianzai", "2008tianzai6_attack1_3", 0.25f);
+//   	LoadSprite("2009daoshi", "2009daoshi3_run_3", 0.25f);
+   	LoadSprite("2010shennv", "2010shennv4_attack1_2", 0.25f);
+//   	LoadSprite("2011dapeng", "2011dapeng1_circlestyle_attack_1", 0.25f);
+//   	LoadSprite("2012fujia", "2012fujia3_bomb_1", 0.25f);
+//   	LoadSprite("2013baji", "2013baji1_attack2_3", 0.25f);
+//   	LoadSprite("2014yingyan", "2014yingyan1_run_3", 0.25f);
+//   	LoadSprite("2022xueying", "xueying_ghosttype_attack_1", 0.25f);
+//   	LoadSprite("2201yini", "2201yini1_attack1_1", 0.25f);
+//   	LoadSprite("2202xiyu", "2202xiyu1_run_3", 0.25f);
+//   	LoadSprite("2203langqi", "2203langqi2_cheer_1", 0.25f);
+//   	LoadSprite("2204wuzhu", "2204wuzhu1_cheer1_1", 0.25f);
+//   	LoadSprite("2205huoyuansu", "2205huoyuansu_summon_1", 0.25f);
+//   	LoadSprite("2206jijian", "2206jijian1_attack5_1", 0.25f);
+//   	LoadSprite("2208tieta", "2208tieta2_attack1_1", 0.25f);
+//   	LoadSprite("2209cunminnv", "cunminnv_run_1", 0.25f);
+//   	LoadSprite("2210cunminnan", "cunminnan_run_1", 0.25f);
+//   	LoadSprite("2212huojuren", "2212huojuren_attack1_3", 0.25f);	
 	dtexf_c3_load_pkg_finish();
 }
 
@@ -59,43 +59,44 @@ void TestAsync::Update()
 {
 	static int count = 0;
 	count++;
- 	if (count == 50) {
- 		dtexf_async_load_spr("2001dashi", "2001dashi1_run_1", "2001dashi.epp");
- 	}
 
-// 	if (count == 50) {
-// 		dtexf_async_load_spr("2001dashi", "2001dashi1_build2_3", "2001dashi.epp");
-// 	} else if (count == 100) {
-// 		dtexf_async_load_spr("2002jumin", "2002jumin1_run_3", "2002jumin.epp");
-// 	} else if (count == 150) {
-// 		dtexf_async_load_spr("2003daoke", "2003daoke5_attack2_2", "2003daoke.epp");
-// 	} else if (count == 200) {
-// 		dtexf_async_load_spr("2004shenshe", "2004shenshe5_attack1_2", "2004shenshe.epp");
-// 	} else if (count == 250) {
-// 		dtexf_async_load_spr("2005mojin", "2005mojin3_run_1", "2005mojin.epp");
-// 		dtexf_async_load_spr("2006jiabing", "2006jiabing5_attack1_3", "2006jiabing.epp");
-// 		dtexf_async_load_spr("2007sishi", "2007sishi5_attack1_3", "2007sishi.epp");
-// 	} else if (count == 300) {
-// 		dtexf_async_load_spr("2008tianzai", "2008tianzai6_attack1_3", "2008tianzai.epp");
-// 		dtexf_async_load_spr("2009daoshi", "2009daoshi3_run_3", "2009daoshi.epp");
-// 		dtexf_async_load_spr("2010shennv", "2010shennv4_attack1_2", "2010shennv.epp");
-// 		dtexf_async_load_spr("2011dapeng", "2011dapeng1_circlestyle_attack_1", "2011dapeng.epp");
-// 		dtexf_async_load_spr("2012fujia", "2012fujia3_bomb_1", "2012fujia.epp");
-// 		dtexf_async_load_spr("2013baji", "2013baji1_attack2_3", "2013baji.epp");
-// 	} else if (count == 350) {
-// 		dtexf_async_load_spr("2014yingyan", "2014yingyan1_run_3", "2014yingyan.epp");
-// 		dtexf_async_load_spr("2022xueying", "xueying_ghosttype_attack_1", "2022xueying.epp");
-// 		dtexf_async_load_spr("2201yini", "2201yini1_attack1_1", "2201yini.epp");
-// 		dtexf_async_load_spr("2202xiyu", "2202xiyu1_run_3", "2202xiyu.epp");
-// 		dtexf_async_load_spr("2203langqi", "2203langqi2_cheer_1", "2203langqi.epp");
-// 		dtexf_async_load_spr("2204wuzhu", "2204wuzhu1_cheer1_1", "2204wuzhu.epp");
-// 		dtexf_async_load_spr("2205huoyuansu", "2205huoyuansu_summon_1", "2205huoyuansu.epp");
-// 		dtexf_async_load_spr("2206jijian", "2206jijian1_attack5_1", "2206jijian.epp");
-// 		dtexf_async_load_spr("2208tieta", "2208tieta2_attack1_1", "2208tieta.epp");
-// 		dtexf_async_load_spr("2209cunminnv", "cunminnv_run_1", "2209cunminnv.epp");
-// 		dtexf_async_load_spr("2210cunminnan", "cunminnan_run_1", "2210cunminnan.epp");
-// 		dtexf_async_load_spr("2212huojuren", "2212huojuren_attack1_3", "2212huojuren.epp");
-// 	}
+ 	if (count == 50) {
+ 		dtexf_async_load_spr("2010shennv", "2010shennv4_attack1_2", "2010shennv.epp");
+	}
+
+//   	if (count == 50) {
+//   		dtexf_async_load_spr("2001dashi", "2001dashi1_run_1", "2001dashi.epp");
+//   	} else if (count == 100) {
+//   		dtexf_async_load_spr("2002jumin", "2002jumin1_run_3", "2002jumin.epp");
+//   	} else if (count == 150) {
+//   		dtexf_async_load_spr("2003daoke", "2003daoke5_attack2_2", "2003daoke.epp");
+//   	} else if (count == 200) {
+//   		dtexf_async_load_spr("2004shenshe", "2004shenshe5_attack1_2", "2004shenshe.epp");
+//   	} else if (count == 250) {
+//   		dtexf_async_load_spr("2005mojin", "2005mojin3_run_1", "2005mojin.epp");
+//   		dtexf_async_load_spr("2006jiabing", "2006jiabing5_attack1_3", "2006jiabing.epp");
+//   		dtexf_async_load_spr("2007sishi", "2007sishi5_attack1_3", "2007sishi.epp");
+//   	} else if (count == 300) {
+//   		dtexf_async_load_spr("2008tianzai", "2008tianzai6_attack1_3", "2008tianzai.epp");
+//   		dtexf_async_load_spr("2009daoshi", "2009daoshi3_run_3", "2009daoshi.epp");
+//   		dtexf_async_load_spr("2010shennv", "2010shennv4_attack1_2", "2010shennv.epp");
+//   		dtexf_async_load_spr("2011dapeng", "2011dapeng1_circlestyle_attack_1", "2011dapeng.epp");
+//   		dtexf_async_load_spr("2012fujia", "2012fujia3_bomb_1", "2012fujia.epp");
+//   		dtexf_async_load_spr("2013baji", "2013baji1_attack2_3", "2013baji.epp");
+//   	} else if (count == 350) {
+//   		dtexf_async_load_spr("2014yingyan", "2014yingyan1_run_3", "2014yingyan.epp");
+//   		dtexf_async_load_spr("2022xueying", "xueying_ghosttype_attack_1", "2022xueying.epp");
+//   		dtexf_async_load_spr("2201yini", "2201yini1_attack1_1", "2201yini.epp");
+//   		dtexf_async_load_spr("2202xiyu", "2202xiyu1_run_3", "2202xiyu.epp");
+//   		dtexf_async_load_spr("2203langqi", "2203langqi2_cheer_1", "2203langqi.epp");
+//   		dtexf_async_load_spr("2204wuzhu", "2204wuzhu1_cheer1_1", "2204wuzhu.epp");
+//   		dtexf_async_load_spr("2205huoyuansu", "2205huoyuansu_summon_1", "2205huoyuansu.epp");
+//   		dtexf_async_load_spr("2206jijian", "2206jijian1_attack5_1", "2206jijian.epp");
+//   		dtexf_async_load_spr("2208tieta", "2208tieta2_attack1_1", "2208tieta.epp");
+//   		dtexf_async_load_spr("2209cunminnv", "cunminnv_run_1", "2209cunminnv.epp");
+//   		dtexf_async_load_spr("2210cunminnan", "cunminnan_run_1", "2210cunminnan.epp");
+//   		dtexf_async_load_spr("2212huojuren", "2212huojuren_attack1_3", "2212huojuren.epp");
+//   	}
 	dtexf_update();
 
 	for (int i = 0, n = m_sprites.size(); i < n; ++i) {
