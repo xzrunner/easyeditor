c4fadb02c948f31bd78483309968805afb1c6aaf
zz <zhuguang@ejoy.com>
2015-09-02 16:27:33 +0800

    [FIXED] ecomplex导入lua格式资源时关闭pre_multi_alpha
    Former-commit-id: fba481ccb49940ae7b86d7ce082eb8a2c6f9d6e2

--- easycomplex/src/libcomplex/FileLoader.cpp
+++ easycomplex/src/libcomplex/FileLoader.cpp
@@ -192,8 +192,10 @@ void FileLoader::LoadLua(const Json::Value& value, const std::string& dir, Symbo
 void FileLoader::LoadImages(const std::string& name, std::vector<d2d::Image*>& images)
 {
 	d2d::SettingData& data = d2d::Config::Instance()->GetSettings();
-	bool old_cfg = data.open_image_edge_clip;
+	bool old_open_image_edge_clip = data.open_image_edge_clip;
 	data.open_image_edge_clip = false;
+	bool old_pre_multi_alpha = data.pre_multi_alpha;
+	data.pre_multi_alpha = false;
 
 	int idx = 1;
 	while (true)
@@ -207,7 +209,8 @@ void FileLoader::LoadImages(const std::string& name, std::vector<d2d::Image*>& i
 		}
 	}
 
-	data.open_image_edge_clip = old_cfg;
+	data.open_image_edge_clip = old_open_image_edge_clip;
+	data.pre_multi_alpha = old_pre_multi_alpha;
 }
 
 void FileLoader::LoadFromNode(const libcoco::IPackNode* node, Symbol* symbol)
