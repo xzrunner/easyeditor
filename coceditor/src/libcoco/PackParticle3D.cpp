842b533e562e965e7b8011df04d0046d2fd0dda0
zz <zhuguang@ejoy.com>
2015-09-09 09:36:37 +0800

    [ADDED] packparticle3d
    Former-commit-id: 29d2163af680fccb4d27eadd2119fe4557820a11

--- coceditor/src/libcoco/PackParticle3D.cpp
+++ coceditor/src/libcoco/PackParticle3D.cpp
@@ -1,1 +1,41 @@
 #include "PackParticle3D.h"
+
+#include "Particle3DToLuaString.h"
+#include "Particle3DFromLua.h"
+
+#include "Particle3DToBin.h"
+#include "Particle3DFromBin.h"
+
+namespace libcoco
+{
+
+void PackParticle3D::PackToLuaString(ebuilder::CodeGenerator& gen, const d2d::TexturePacker& tp) const
+{
+	return Particle3DToLuaString::Pack(this, gen);
+}
+
+void PackParticle3D::UnpackFromLua(lua_State* L, const std::vector<d2d::Image*>& images)
+{
+}
+
+int PackParticle3D::SizeOfPackToBin() const
+{
+	return 0;
+}
+
+void PackParticle3D::PackToBin(uint8_t** ptr, const d2d::TexturePacker& tp) const
+{
+
+}
+
+int PackParticle3D::SizeOfUnpackFromBin() const
+{
+	return 0;
+}
+
+void PackParticle3D::UnpackFromBin(uint8_t** ptr, const std::vector<d2d::Image*>& images)
+{
+
+}
+
+}
