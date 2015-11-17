dc8bda20f40e83ec3c580a049248f7644d632851
zz <zhuguang@ejoy.com>
2015-11-17 18:34:25 +0800

    [FIXED] 文字打包
    Former-commit-id: e496a8a59e640ce75f9daa033ea04d661092b4eb

--- easyrespacker/src/librespacker/LabelToBin.cpp
+++ easyrespacker/src/librespacker/LabelToBin.cpp
@@ -49,12 +49,14 @@ void LabelToBin::Pack(const PackLabel* label, uint8_t** ptr)
 	uint8_t font_size = label->font_size;
 	pack(font_size, ptr);
 	uint32_t font_color = d2d::trans_color2int(label->font_color, d2d::PT_RGBA);
+	pack(font_color, ptr);
 
 	uint8_t edge = label->edge ? 1 : 0;
 	pack(edge, ptr);
 	uint16_t edge_size = (int)(label->edge_size * 1024);
 	pack(edge_size, ptr);
 	uint32_t edge_color = d2d::trans_color2int(label->edge_color, d2d::PT_RGBA);
+	pack(edge_color, ptr);
 
 	uint8_t align_hori = label->align_hori;
 	pack(align_hori, ptr);
@@ -63,6 +65,8 @@ void LabelToBin::Pack(const PackLabel* label, uint8_t** ptr)
 
 	uint16_t space_hori = (int)(label->space_hori * 1024);
 	uint16_t space_vert = (int)(label->space_vert * 1024);
+	pack(space_hori, ptr);
+	pack(space_vert, ptr);
 
 	pack_str(label->text, ptr);
 	pack_str(label->tid, ptr);
--- easyrespacker/src/librespacker/LabelToLuaString.cpp
+++ easyrespacker/src/librespacker/LabelToLuaString.cpp
@@ -24,12 +24,12 @@ void LabelToLuaString::Pack(const PackLabel* label, ebuilder::CodeGenerator& gen
 	lua::connect(gen, 3, 
 		lua::assign("font", label->font), 
 		lua::assign("font_size", label->font_size),
-		lua::assign("font_color", d2d::transColor(label->font_color, d2d::PT_BGRA)));
+		lua::assign("font_color", d2d::transColor(label->font_color, d2d::PT_RGBA)));
 
 	lua::connect(gen, 3, 
 		lua::assign("edge", label->edge), 
 		lua::assign("edge_size", label->edge_size),
-		lua::assign("edge_color", d2d::transColor(label->edge_color, d2d::PT_BGRA)));
+		lua::assign("edge_color", d2d::transColor(label->edge_color, d2d::PT_RGBA)));
 
 	lua::connect(gen, 2, 
 		lua::assign("align_hori", label->align_hori), 
@@ -40,8 +40,8 @@ void LabelToLuaString::Pack(const PackLabel* label, ebuilder::CodeGenerator& gen
 		lua::assign("space_vert", label->space_vert));
 
 	lua::connect(gen, 2, 
-		lua::assign("text", label->text), 
-		lua::assign("tid", label->tid));
+		lua::assign("text", "\"" + label->text + "\""),
+		lua::assign("tid", "\"" + label->tid + "\""));
 
 	gen.detab();
 	gen.line("},");
