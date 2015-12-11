e3ad609fa36d17e2f4a2a80fe0212ddb6407dbf6
zz <zhuguang@ejoy.com>
2015-12-11 16:49:16 +0800

    up gtxt
    Former-commit-id: 28620ad161f62349d4b9c913eec3795dc0708956

--- easytext/src/libtext/GTxt.cpp
+++ easytext/src/libtext/GTxt.cpp
@@ -202,7 +202,7 @@ void GTxt::Init()
 
 	gtxt_glyph_init(50, 500, NULL);
 
-	gtxt_richtext_ext_sym_cb_init(&ext_sym_create, &ext_sym_release, &ext_sym_get_size, &ext_sym_render);
+	gtxt_richtext_ext_sym_cb_init(&ext_sym_create, &ext_sym_release, &ext_sym_get_size, &ext_sym_render, NULL);
 }
 
 }
