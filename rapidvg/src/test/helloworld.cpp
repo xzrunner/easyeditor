5d323672aa6ddb104afdadee0ef1cccf04afad0f
zz <zhuguang@ejoy.com>
2016-01-17 09:15:44 +0800

    up rapidvg
    Former-commit-id: 474b61564ec6075341bb4dd1467a7339489eec4b

--- rapidvg/src/test/helloworld.cpp
+++ rapidvg/src/test/helloworld.cpp
@@ -1,31 +1,29 @@
 #include "rvg_render.h"
-#include "rvg_shader2.h"
 
 #include <sl_shader.h>
+#include <sl_shape.h>
 
 #include <gl/glew.h>
 #include <gl/freeglut.h>
 
 void 
 display(void) {
-	rvg_shader2_bind();
-
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	rvg_shader2_color(0xff0000ff);
+	sl_shape_color(0xff0000ff);
 	rvg_point(0, 0, 5);
-	rvg_shader2_color(0xffff00ff);
+	sl_shape_color(0xffff00ff);
 	rvg_line(-100, -100, -50, -80);
-	rvg_shader2_color(0xffffffff);
+	sl_shape_color(0xffffffff);
 	rvg_rect(10, 10, 100, 100, true);
 
 	rvg_circle(100, -100, 50, false);
 
-	rvg_shader2_color(0xff00ffff);
+	sl_shape_color(0xff00ffff);
 	rvg_set_circle_segments(32);
 	rvg_circle(200, -100, 50, true);
 
-	rvg_shader2_flush();
+	sl_shader_flush();
 
 	glFlush();
 }
@@ -35,18 +33,17 @@ init(void) {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 
 	sl_shader_init();
+	sl_shape_init();
 
-	rvg_shader2_init();
-// 	rvg_shader_load();
-// 	rvg_shader_bind();
-
-	rvg_shader2_modelview(0, 0, 1, 1);
+	sl_shape_modelview(0, 0, 1, 1);
 }
 
 void 
 resize(int width, int height) {
 	glViewport(0, 0, width, height);
-	rvg_shader2_projection(width, height);
+	sl_shape_projection(width, height);
+
+	sl_shape_bind();
 }
 
 int 
