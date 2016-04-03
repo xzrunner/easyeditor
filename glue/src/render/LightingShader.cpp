598c8e25092dfef453cca52d117d6e8f6876e603
zz <zhuguang@ejoy.com>
2016-04-03 09:12:19 +0800

    up shaderlab
    Former-commit-id: 9f320b007a377aacb5620263481ba9b1e39e47fd

--- glue/src/render/LightingShader.cpp
+++ glue/src/render/LightingShader.cpp
@@ -1,6 +1,6 @@
 #include "LightingShader.h"
 
-#include <sl_lighting.h>
+#include <sl_model3.h>
 #include <m3_model.h>
 #include <ds_array.h>
 
@@ -20,32 +20,32 @@ LightingShader::~LightingShader()
 
 void LightingShader::Load()
 {
-	sl_lighting_load();
+	sl_model3_load();
 }
 
 void LightingShader::Unload()
 {
-	sl_lighting_unload();
+	sl_model3_unload();
 }
 
 void LightingShader::Bind()
 {
-	sl_lighting_bind();
+	sl_model3_bind();
 }
 
 void LightingShader::Unbind()
 {
-	sl_lighting_unbind();
+	sl_model3_unbind();
 }
 
 void LightingShader::OnSize(int width, int height)
 {
-	sl_lighting_projection((float)width / height);
+	sl_model3_projection((float)width / height);
 }
 
 void LightingShader::Commit()
 {
-	sl_lighting_commit();
+	sl_model3_commit();
 }
 
 void LightingShader::Reset()
@@ -54,33 +54,33 @@ void LightingShader::Reset()
 
 void LightingShader::SetProjection(int width, int height)
 {
-	sl_lighting_projection((float)width / height);
+	sl_model3_projection((float)width / height);
 }
 
 void LightingShader::SetModelView(const sm_mat4& mat)
 {
 	m_mat = mat;
-	sl_lighting_modelview(&m_mat);
+	sl_model3_modelview(&m_mat);
 }
 
 void LightingShader::SetNormalMatrix(const sm_mat3& mat)
 {
-	sl_lighting_set_normal_matrix(&mat);
+	sl_model3_set_normal_matrix(&mat);
 }
 
 void LightingShader::SetLightPosition(const vec3& pos)
 {
 	sm_vec3 sm_pos;
 	memcpy(&sm_pos.x, pos.xyz, sizeof(sm_pos));
-	sl_lighting_set_light_position(&sm_pos);
+	sl_model3_set_light_position(&sm_pos);
 }
 
 void LightingShader::Draw(m3_model* model)
 {
 	for (int i = 0, n = ds_array_size(model->meshes); i < n; ++i) {
 		m3_mesh* mesh = *(m3_mesh**)ds_array_fetch(model->meshes, i);
-		sl_lighting_set_material(&mesh->material.ambient, &mesh->material.diffuse, &mesh->material.specular, mesh->material.shininess);
-		sl_lighting_draw(mesh->vertices, mesh->indices);
+		sl_model3_set_material(&mesh->material.ambient, &mesh->material.diffuse, &mesh->material.specular, mesh->material.shininess);
+		sl_model3_draw(mesh->vertices, mesh->indices);
 	}
 }
 
