501ed1f24839e905077194185eb1f75425c09f81
zz <zhuguang@ejoy.com>
2016-05-06 17:06:15 +0800

    [FIXED] pack animation, text used in multi actions
    Former-commit-id: f99b034d2477d1ce01740d7b311e7f2714ba115e

--- easyrespacker/src/librespacker/PackAnimation.cpp
+++ easyrespacker/src/librespacker/PackAnimation.cpp
@@ -124,7 +124,8 @@ bool PackAnimation::AddComponent(const IPackNode* node, const std::string& name,
 				ee::FileType::Type type = ee::FileType::GetType(node->GetFilepath());
 				if (type == ee::FileType::e_image ||
 					type == ee::FileType::e_complex ||
-					type == ee::FileType::e_anim) {
+					type == ee::FileType::e_anim ||
+					type == ee::FileType::e_text) {
 					comp_idx = i;
 					return true;
 				}
