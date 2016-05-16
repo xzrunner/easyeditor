a7417d7bd99b912cd017f162ad2dda9e69edb946
zz <zhuguang@ejoy.com>
2016-05-16 18:49:53 +0800

    [FIXED] rgb hsl trans
    Former-commit-id: a4b6ae8bb505e50b46a656a00a8f96e6b6602402

--- easyeditor/source/data/primitive/convert_color.cpp
+++ easyeditor/source/data/primitive/convert_color.cpp
@@ -1,4 +1,4 @@
-// copy from http://www.geekymonkey.com/Programming/CSharp/RGB2HSL_HSL2RGB.htm
+// code from http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
 
 #include "convert_color.h"
 
@@ -7,88 +7,48 @@
 namespace ee
 {
 
+static inline float hue2rgb(float p, float q, float t) 
+{
+	if (t < 0) t += 1;
+	if (t > 1) t -= 1;
+	if (t < 1.0f / 6.0f) return p + (q - p) * 6 * t;
+	if (t < 1.0f / 2.0f) return q;
+	if (t < 2.0f / 3.0f) return p + (q - p) * (2.0f / 3.0f - t) * 6;
+	return p;
+}
+
 void hsl2rgb(float h, float s, float l, float& r, float& g, float& b)
 {
-	r = g = b = l;		// default to gray
-
-	float v = (l <= 0.5f) ? (l * (1.0f + s)) : (l + s - l * s);
-	if (v <= 0) {
-		return;
-	}
-
-	float m = l + l - v;
-	float sv = (v - m ) / v;
-	h *= 6.0f;
-	int sextant = (int)h;
-	float fract = h - sextant;
-	float vsf = v * sv * fract;
-	float mid1 = m + vsf;
-	float mid2 = v - vsf;
-	switch (sextant)
-	{
-	case 0:
-		r = v;
-		g = mid1;
-		b = m;
-		break;
-	case 1:
-		r = mid2;
-		g = v;
-		b = m;
-		break;
-	case 2:
-		r = m;
-		g = v;
-		b = mid1;
-		break;
-	case 3:
-		r = m;
-		g = mid2;
-		b = v;
-		break;
-	case 4:
-		r = mid1;
-		g = m;
-		b = v;
-		break;
-	case 5:
-		r = v;
-		g = m;
-		b = mid2;
-		break;
+	if (s == 0) {
+		r = g = b = 1;
+	} else {
+		float q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		float p = 2 * l - q;
+		r = hue2rgb(p, q, h + 1.0f / 3.0f);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1.0f / 3.0f);
 	}
 }
 
 void rgb2hsl(float r, float g, float b, float& h, float& s, float& l)
 {
-	h = s = l = 0;		// default to black
-
-	float v = std::max(std::max(r, g), b);
-	float m = std::min(std::min(r, g), b);
-	l = (m + v) / 2.0f;
-	if (l <= 0.0) {
-		return;
-	}
-	float vm = v - m;
-	s = vm;
-	if (s > 0.0f) {
-		s /= (l <= 0.5f) ? (v + m ) : (2.0f - v - m) ;
+	float max = std::max(std::max(r, g), b),
+		  min = std::min(std::min(r, g), b);
+	h = s = l = (max + min) / 2;
+	if (max == min) {
+		h = s = 0;
 	} else {
-		return;
+		float d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+		if (max == r) {
+			h = (g - b) / d + (g < b ? 6 : 0);
+		} else if (max == g) {
+			h = (b - r) / d + 2;
+		} else if (max == b) {
+			h = (r - g) / d + 4;
+		}
+		h /= 6;
 	}
-	float r2 = (v - r) / vm;
-	float g2 = (v - g) / vm;
-	float b2 = (v - b) / vm;
-	if (r == v) {
-		h = (g == m ? 5.0f + b2 : 1.0f - g2);
-	} else if (g == v) {
-		h = (b == m ? 1.0f + r2 : 3.0f - b2);
-	} else {
-		h = (r == m ? 3.0f + g2 : 5.0f - r2);
-	}
-	h /= 6.0;
-
-	return;
 }
 
 }
