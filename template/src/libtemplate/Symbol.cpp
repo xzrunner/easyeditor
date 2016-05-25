7afa322b868fba173b7709ad848ef65418fd2470
zz <zhuguang@ejoy.com>
2016-05-25 13:20:46 +0800

    [FIXED] easytemplate
    Former-commit-id: 3b97a8ad4246d116fd71922efd71cefa19ba5479

--- template/src/libtemplate/Symbol.cpp
+++ template/src/libtemplate/Symbol.cpp
@@ -18,7 +18,7 @@ void Symbol::Draw(const s2::RenderParams& params, const ee::Sprite* spr) const
 
 sm::rect Symbol::GetSize(const ee::Sprite* sprite) const
 {
-	return sm::rect(200, 200);
+	return sm::rect(sm::vec2(0, 0), 200, 200);
 }
 
 void Symbol::LoadResources()
