b099ee5503446da5d4e201968252a954c57a25d3
zz <zhuguang@ejoy.com>
2016-05-25 08:24:50 +0800

    [FIXED] avoid implicit conversions for Color
    Former-commit-id: 7ad753faf8d7879bc5f82ae7432bca12de5b9712

--- sprite2/include/sprite2/Color.h
+++ sprite2/include/sprite2/Color.h
@@ -17,7 +17,7 @@ public:
 public:
 	Color();
 	Color(uint32_t rgba);
-	Color(int r, int g, int b, int a = 255);
+	explicit Color(int r, int g, int b, int a = 255);
 
 	bool operator != (const Color& col) const;
 	bool operator == (const Color& col) const;
@@ -35,10 +35,6 @@ public:
 	uint32_t ToRGBA() const;
 	void FromRGBA(uint32_t rgba);
 	
-private:
-	Color(float r, float g, float b, float a) {}
-	Color(float r, float g, float b) {}
-
 }; // Color
 
 }
