14609130c66a0da8078c446e0a6dea9495f34a47
zz <zhuguang@ejoy.com>
2016-05-30 19:23:59 +0800

    [FIXED] io for filter
    Former-commit-id: 7ad9455b8eed5b48ec36799eb3da6239eebaca1d

--- easyeditor/source/data/sprite/SpriteIO.cpp
+++ easyeditor/source/data/sprite/SpriteIO.cpp
@@ -10,6 +10,8 @@
 #include <sprite2/RenderCamera.h>
 #include <sprite2/FilterFactory.h>
 #include <sprite2/RenderFilter.h>
+#include <sprite2/RFEdgeDetection.h>
+#include <sprite2/RFGaussianBlur.h>
 
 #include <assert.h>
 
@@ -62,29 +64,81 @@ void SpriteIO::StoreColor(Json::Value& val, const s2::RenderColor& color)
 
 void SpriteIO::LoadShader(const Json::Value& val, s2::RenderShader& shader)
 {
-	if (!val["blend"].isNull()) {
-		std::string disc = val["blend"].asString();
-		if (Config::Instance()->IsRenderOpen()) {
-			shader.blend = BlendModes::Instance()->GetModeFromNameEN(disc);
-		}
+	if (!Config::Instance()->IsRenderOpen()) {
+		return;
 	}
 
-	if (!val["filter"].isNull()) {
-		std::string disc = val["filter"].asString();
-		if (Config::Instance()->IsRenderOpen()) {
+	if (!val["blend"].isNull()) {
+		std::string disc = val["blend"].asString();
+		shader.blend = BlendModes::Instance()->GetModeFromNameEN(disc);
+	}
+
+	if (!val["filter"].isNull()) 
+	{
+		if (shader.filter) {
+			delete shader.filter;
+		}
+
+		if (val["filter"].isString()) 
+		{
+			std::string disc = val["filter"].asString();
 			s2::FilterMode filter = FilterModes::Instance()->GetModeFromNameEN(disc);
-			assert(!shader.filter);
 			shader.filter = s2::FilterFactory::Instance()->Create(filter);
+		} 
+		else 
+		{
+			const Json::Value& fval = val["filter"];
+			std::string disc = fval["mode"].asString();
+			s2::FilterMode filter = FilterModes::Instance()->GetModeFromNameEN(disc);
+			shader.filter = s2::FilterFactory::Instance()->Create(filter);
+			switch (filter)
+			{
+			case s2::FM_EDGE_DETECTION:
+				{
+					float blend = (float)(fval["blend"].asDouble());
+					s2::RFEdgeDetection* filter = static_cast<s2::RFEdgeDetection*>(shader.filter);
+					filter->SetBlend(blend);
+				}
+				break;
+			case s2::FM_GAUSSIAN_BLUR:
+				{
+					int iterations = fval["iterations"].asInt();
+					s2::RFGaussianBlur* filter = static_cast<s2::RFGaussianBlur*>(shader.filter);
+					filter->SetIterations(iterations);
+				}
+				break;
+			}
 		}
 	}
 }
 
 void SpriteIO::StoreShader(Json::Value& val, const s2::RenderShader& shader)
 {
-	if (Config::Instance()->IsRenderOpen()) {
-		val["blend"] = BlendModes::Instance()->GetNameENFromMode(shader.blend);
-		val["filter"] = FilterModes::Instance()->GetNameENFromMode(shader.filter->GetMode());
+	if (!Config::Instance()->IsRenderOpen()) {
+		return;
 	}
+
+	val["blend"] = BlendModes::Instance()->GetNameENFromMode(shader.blend);
+
+	Json::Value fval;
+	s2::FilterMode mode = shader.filter->GetMode();
+	fval["mode"] = FilterModes::Instance()->GetNameENFromMode(mode);
+	switch (mode)
+	{
+	case s2::FM_EDGE_DETECTION:
+		{
+			s2::RFEdgeDetection* filter = static_cast<s2::RFEdgeDetection*>(shader.filter);
+			fval["blend"] = filter->GetBlend();
+		}
+		break;
+	case s2::FM_GAUSSIAN_BLUR:
+		{
+			s2::RFGaussianBlur* filter = static_cast<s2::RFGaussianBlur*>(shader.filter);
+			fval["iterations"] = filter->GetIterations();
+		}
+		break;
+	}
+	val["filter"] = fval;
 }
 
 void SpriteIO::LoadCamera(const Json::Value& val, s2::RenderCamera& camera)
