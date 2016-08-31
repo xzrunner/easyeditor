399b74754c239e2aa7f319b80027f8104bcd0ea1
zz <zhuguang@ejoy.com>
2016-08-31 20:37:43 +0800

    [FIXED] offset center absorb
    Former-commit-id: cdd3e17010f25996c77fd5a474b4b074191f1ae2

--- easyeditor/source/edit/op/OffsetSpriteState.cpp
+++ easyeditor/source/edit/op/OffsetSpriteState.cpp
@@ -5,6 +5,8 @@
 #include "Math2D.h"
 #include "SpriteCtrlNode.h"
 #include "ArrangeSpriteImpl.h"
+#include "CameraMgr.h"
+#include "Camera.h"
 
 #include <SM_Calc.h>
 
@@ -26,13 +28,20 @@ OffsetSpriteState::~OffsetSpriteState()
 
 void OffsetSpriteState::OnMouseRelease(const sm::vec2& pos)
 {
+	float s = CameraMgr::Instance()->GetCamera()->GetScale();
+	float r = ArrangeSpriteImpl::CTRL_NODE_RADIUS * s * 2;
+
 	sm::vec2 ctrl_nodes[8];
 	SpriteCtrlNode::GetSpriteCtrlNodes(m_spr, ctrl_nodes);
 	sm::vec2 fixed = pos;
-	for (int i = 0; i < 8; ++i) {
-		if (sm::dis_pos_to_pos(fixed, ctrl_nodes[i]) < ArrangeSpriteImpl::CTRL_NODE_RADIUS) {
-			fixed = ctrl_nodes[i];
-			break;
+	if (sm::dis_pos_to_pos(fixed, m_spr->GetPosition()) < r) {
+		fixed = m_spr->GetPosition();
+	} else {
+		for (int i = 0; i < 8; ++i) {
+			if (sm::dis_pos_to_pos(fixed, ctrl_nodes[i]) < r) {
+				fixed = ctrl_nodes[i];
+				break;
+			}
 		}
 	}
 
