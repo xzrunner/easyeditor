cdb6479f312fbd728101515d689c4a491e311b03
zz <zhuguang@ejoy.com>
2016-12-02 15:36:10 +0800

    [FIXED] FileNameProperty open
    Former-commit-id: d6c0906af21e831372ced17471a149c38410b23e

--- easyeditor/source/gui/FileNameProperty.cpp
+++ easyeditor/source/gui/FileNameProperty.cpp
@@ -1,5 +1,9 @@
 #include "FileNameProperty.h"
 #include "FileHelper.h"
+#include "SymbolFile.h"
+#include "SymbolType.h"
+
+#include <sprite2/SymType.h>
 
 namespace ee
 {
@@ -14,8 +18,44 @@ FileNameProperty::FileNameProperty(const wxString& label, const wxString& name, 
 
 bool FileNameProperty::OnButtonClick(wxPropertyGrid* propgrid, wxString& value)
 {
-	std::string command = "easycomplex.exe " + m_filepath;
-	wxExecute(command, wxEXEC_ASYNC, NULL);
+	int type = SymbolFile::Instance()->Type(m_filepath.ToStdString());
+	switch (type)
+	{
+	case s2::SYM_COMPLEX:
+		wxExecute("easycomplex.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	case s2::SYM_ANIMATION:
+		wxExecute("easyanimation.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	case s2::SYM_ICON:
+		wxExecute("easyicon.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	case s2::SYM_MASK:
+		wxExecute("easymask.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	case s2::SYM_MESH:
+		wxExecute("easymesh.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	case s2::SYM_PARTICLE3D:
+		wxExecute("easyparticle3d.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	case s2::SYM_PARTICLE2D:
+		wxExecute("easyparticle2d.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	case s2::SYM_SCALE9:
+		wxExecute("easyscale9.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	case s2::SYM_TEXTURE:
+		wxExecute("easytexture.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	case s2::SYM_TRAIL:
+		wxExecute("easytrail.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	case ee::SYM_UI: case ee::SYM_UIWND:
+		wxExecute("easyui.exe " + m_filepath, wxEXEC_ASYNC, NULL);
+		break;
+	}
+
 	return false;
 }
 
