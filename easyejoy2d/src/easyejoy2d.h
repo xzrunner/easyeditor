f60cdedd39bdcebde3d12af29fb0329f4aa30b5d
zz <zhuguang@ejoy.com>
2016-12-26 11:25:58 +0800

    rm easyejoy2d
    Former-commit-id: fd69208a3d703a66779cf50e1c38196368d4a5db

--- easyejoy2d/src/easyejoy2d.h
+++
@@ -1,7 +0,0 @@
-#include "easyejoy2d/EJScreen.h"
-#include "easyejoy2d/EJSprite.h"
-#include "easyejoy2d/EJShader.h"
-
-#include "easyejoy2d/Sprite.h"
-#include "easyejoy2d/Symbol.h"
-#include "easyejoy2d/LibraryPage.h"
