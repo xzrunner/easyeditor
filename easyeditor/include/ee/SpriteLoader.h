7e354b052cb8adf4873d2c938bc423ff9f07785f
zz <zhuguang@ejoy.com>
2017-06-13 23:06:42 +0800

    [FIXED] spr loader
    Former-commit-id: adb3200037c1eb00cfa3735a28778f297776fe17

--- easyeditor/include/ee/SpriteLoader.h
+++ easyeditor/include/ee/SpriteLoader.h
@@ -21,7 +21,7 @@ public:
 	 */	
 	virtual s2::Sprite* Create(s2::Symbol* sym) const;
 	virtual s2::Sprite* Create(const std::string& filepath) const;
-	virtual s2::Sprite* Create(const Json::Value& val, const std::string& dir) const;
+	virtual s2::Sprite* Create(const Json::Value& val, const std::string& dir, bool flatten = false) const;
 
 protected:
 	virtual std::string GetSymbolPath(const std::string& dir, const Json::Value& val) const;
