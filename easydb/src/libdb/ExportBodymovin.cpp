04e41c9de0b30d00d901e028ee796413ee6a9cd9
zz <zhuguang@ejoy.com>
2017-07-24 16:39:33 +0800

    [FIXED] asset with unknown comp
    Former-commit-id: fe27c3e479a2b3003b8307b0ca720a143be4aa98

--- easydb/src/libdb/ExportBodymovin.cpp
+++ easydb/src/libdb/ExportBodymovin.cpp
@@ -68,23 +68,61 @@ void ExportBodymovin::Trigger(const std::string& src_file, const std::string& ds
 
 	std::map<std::string, s2::Sprite*> map_assets;
 	const std::vector<gum::BodymovinParser::Asset>& assets = parser.GetAssets();
-	for (int i = 0, n = assets.size(); i < n; ++i)
+	std::vector<bool> flags(assets.size(), false);
+	while (true)
 	{
-		const gum::BodymovinParser::Asset& a = assets[i];
-		if (a.layers.empty()) 
+		bool fail = false;
+		for (int i = 0, n = assets.size(); i < n; ++i)
 		{
-			std::string filepath = gum::FilepathHelper::Absolute(".", a.filepath);
-			s2::Sprite* spr = spr_loader.Create(filepath);
-			map_assets.insert(std::make_pair(a.id, spr));
+			if (flags[i]) {
+				continue;
+			}			
+			const gum::BodymovinParser::Asset& a = assets[i];
+			if (a.layers.empty()) 
+			{
+				std::string filepath = gum::FilepathHelper::Absolute(".", a.filepath);
+				s2::Sprite* spr = spr_loader.Create(filepath);
+				map_assets.insert(std::make_pair(a.id, spr));
+				flags[i] = true;
+			}
+			else
+			{
+				bool skip = false;
+				for (int j = 0, m = a.layers.size(); j < m; ++j) 
+				{
+					const gum::BodymovinParser::Layer& layer = a.layers[j];
+					if (layer.layer_type == gum::BodymovinParser::LAYER_SOLID) {
+						continue;
+					}
+					const std::string& id = a.layers[j].ref_id;
+					std::map<std::string, s2::Sprite*>::iterator itr = map_assets.find(id);
+					if (itr == map_assets.end()) {
+						skip = true;
+						break;
+					}
+				}
+				flags[i] = !skip;
+				if (skip) {
+					fail = true;
+					continue;
+				}
+
+				libanim::Symbol* sym = new libanim::Symbol();
+				gum::BodymovinAnimLoader loader(sym, &sym_loader, &spr_loader);
+				loader.LoadLayers(map_assets, a.layers, parser.GetFrameRate(), parser.GetWidth(), parser.GetHeight());
+				std::string filepath = dst_dir + "\\" + a.id + "_" + ee::SymbolFile::Instance()->Tag(s2::SYM_ANIMATION) + ".json";
+				libanim::FileSaver::Store(filepath, *sym);
+
+				sym->SetFilepath(filepath);
+				libanim::Sprite* spr = new libanim::Sprite(sym);
+				spr->UpdateBounding();
+				map_assets.insert(std::make_pair(a.id, spr));
+
+				sym->RemoveReference();
+			}
 		}
-		else
-		{
-			libanim::Symbol* sym = new libanim::Symbol();
-			gum::BodymovinAnimLoader loader(sym, &sym_loader, &spr_loader);
-			loader.LoadLayers(map_assets, a.layers, parser.GetFrameRate(), parser.GetWidth(), parser.GetHeight());
-			std::string filepath = dst_dir + "\\" + a.id + "_" + ee::SymbolFile::Instance()->Tag(s2::SYM_ANIMATION) + ".json";
-			libanim::FileSaver::Store(filepath, *sym);
-			sym->RemoveReference();
+		if (!fail) {
+			break;
 		}
 	}
 
