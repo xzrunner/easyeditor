2f0c0558e4d2b5249b6e45110bfb9b040f518357
zz <zhuguang@ejoy.com>
2017-11-09 22:14:30 +0800

    [FIXED] shaderlab init
    Former-commit-id: 8d717bbcb96ecf573e7eb7f21d55db3bac1dc167

--- easydb/src/libdb/utility.cpp
+++ easydb/src/libdb/utility.cpp
@@ -3,6 +3,17 @@
 #include <ee/EE_DTex.h>
 #include <ee/EE_GTxt.h>
 #include <gum/ShaderLab.h>
+#include <gum/RenderContext.h>
+
+#include <shaderlab/ShaderMgr.h>
+#include <shaderlab/Shape2Shader.h>
+#include <shaderlab/Shape3Shader.h>
+#include <shaderlab/Sprite2Shader.h>
+#include <shaderlab/Sprite3Shader.h>
+#include <shaderlab/BlendShader.h>
+#include <shaderlab/FilterShader.h>
+#include <shaderlab/MaskShader.h>
+#include <shaderlab/Model3Shader.h>
 
 #include <gl/glew.h>
 #include <glfw.h>
@@ -23,7 +34,22 @@ int init_gl()
 		return -2;
 	}
 
-	gum::ShaderLab::Instance()->Init();
+//	gum::ShaderLab::Instance()->Init();
+	{
+		sl::ShaderMgr* mgr = sl::ShaderMgr::Instance();
+		ur::RenderContext* rc = gum::RenderContext::Instance()->GetImpl();
+		mgr->SetContext(rc);
+
+		mgr->CreateShader(sl::SHAPE2, new sl::Shape2Shader(rc));
+		mgr->CreateShader(sl::SHAPE3, new sl::Shape3Shader(rc));
+		mgr->CreateShader(sl::SPRITE2, new sl::Sprite2Shader(rc));
+		mgr->CreateShader(sl::SPRITE3, new sl::Sprite3Shader(rc));
+		mgr->CreateShader(sl::BLEND, new sl::BlendShader(rc));
+		mgr->CreateShader(sl::FILTER, new sl::FilterShader(rc));
+		mgr->CreateShader(sl::MASK, new sl::MaskShader(rc));
+		mgr->CreateShader(sl::MODEL3, new sl::Model3Shader(rc));
+	}
+
 	ee::DTex::Init();
 	ee::GTxt::Init();
 
