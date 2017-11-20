14c85217b99d553309ce6c0af0ec1f76ac84944c
zz <zhuguang@ejoy.com>
2017-11-20 10:34:44 +0800

    [FIXED] pack no filepath sym, set force_curr
    Former-commit-id: c4e1adaf5e653a84c90f3c7fa01c13084617f078

--- easysprpacker/src/libsprpacker/PackNode.cpp
+++ easysprpacker/src/libsprpacker/PackNode.cpp
@@ -2,6 +2,8 @@
 #include "PackIDMgr.h"
 #include "typedef.h"
 
+#include <ee/SymbolLoader.h>
+
 #include <simp/NodeFactory.h>
 
 namespace esprpacker
@@ -25,6 +27,9 @@ void PackNode::SetFilepath(const std::string& filepath) const
 
 void PackNode::SetID(const std::string& filepath, bool force_curr) const
 {
+	if (ee::SymbolLoader::IsNoFilepathSym(filepath)) {
+		force_curr = true;
+	}
 	PackIDMgr::Instance()->QueryID(filepath, m_pkg_id, m_node_id, m_filepath != SPRITE_FILEPATH, force_curr);
 }
 
