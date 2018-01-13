1e4b84e2797fced2d84730c4292e6f57b31eeb84
zz <zhuguang@ejoy.com>
2018-01-13 22:14:51 +0800

    rename

--- easynode3d/src/easynode3d/CamZoomState.cpp
+++ easynode3d/src/easynode3d/CamZoomState.cpp
@@ -15,9 +15,9 @@ void CamZoomState::OnMouseWheelRotation(int x, int y, int direction)
 	sm::vec3 dir = m_canvas.TransPos3ScreenToDir(sm::vec2(x, y));
 	static const float OFFSET = 0.1f;
 	if (direction > 0) {
-		m_cam.MoveToward(dir, m_cam.GetDistance() * OFFSET);
+		m_cam.Move(dir, m_cam.GetDistance() * OFFSET);
 	} else {
-		m_cam.MoveToward(dir, - m_cam.GetDistance() * OFFSET);
+		m_cam.Move(dir, - m_cam.GetDistance() * OFFSET);
 	}
 
 	m_canvas.Refresh();
