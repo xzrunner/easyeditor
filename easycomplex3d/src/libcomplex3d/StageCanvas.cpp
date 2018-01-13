a01eb9150efa40d37d1c62dd639fc2870b41943a
zz <zhuguang@ejoy.com>
2018-01-13 16:45:43 +0800

    [CHANGED] draw bg

--- easycomplex3d/src/libcomplex3d/StageCanvas.cpp
+++ easycomplex3d/src/libcomplex3d/StageCanvas.cpp
@@ -15,8 +15,23 @@ StageCanvas::StageCanvas(wxWindow* stage_wnd, ee::EditPanelImpl* stage,
 
 void StageCanvas::DrawBackground() const
 {
-	n3::PrimitiveDraw::SetColor(ee::LIGHT_RED.ToABGR());
-	n3::PrimitiveDraw::Cross(sm::vec3(0, 0, 0), sm::vec3(10, 10, 10));
+	n3::PrimitiveDraw::SetColor(0xff000088);
+	n3::PrimitiveDraw::Cross(sm::vec3(0, 0, 0), sm::vec3(100, 100, 100));
+
+	static const int TOT_LEN = 100;
+	static const int GRID_EDGE = 5;
+	for (int y = -TOT_LEN; y < TOT_LEN; y += GRID_EDGE) {
+		for (int x = -TOT_LEN; x < TOT_LEN; x += GRID_EDGE) {
+			if ((x + y) % (GRID_EDGE * 2) == 0) {
+				n3::PrimitiveDraw::SetColor(0xff444444);
+			} else {
+				n3::PrimitiveDraw::SetColor(0xff888888);
+			}
+			n3::PrimitiveDraw::Rect(
+				sm::vec3(x, y, 0),
+				sm::vec3(x + GRID_EDGE, y + GRID_EDGE, 0));
+		}
+	}
 }
 
 }
