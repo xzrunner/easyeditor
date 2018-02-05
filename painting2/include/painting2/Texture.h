409fd7d6be9809f39387e427f9197d0663378c3c
zz <zhuguang@ejoy.com>
2018-02-05 16:20:06 +0800

    [ADDED] Texture's m_format

--- painting2/include/painting2/Texture.h
+++ painting2/include/painting2/Texture.h
@@ -10,9 +10,10 @@ namespace pt2
 class Texture
 {
 public:
-	Texture(uint16_t w, uint16_t h, uint32_t id);
+	Texture(uint16_t w, uint16_t h, uint32_t id, int format);
+	virtual ~Texture() {}
 
-	void Init(uint16_t w, uint16_t h, uint32_t id);
+	void Init(uint16_t w, uint16_t h, uint32_t id, int format);
 	void InitOri(uint16_t ori_w, uint16_t ori_h);
 	
 	sm::vec2 GetSize() const {
@@ -21,6 +22,11 @@ public:
 	sm::vec2 GetOriSize() const {
 		return sm::vec2(m_ori_w, m_ori_h);
 	}
+
+	int GetWidth() const { return m_width; }
+	int GetHeight() const { return m_height; }
+
+	int GetFormat() const { return m_format; }
 
 	uint32_t GetTexID() const { return m_tex_id; }
 
@@ -31,6 +37,8 @@ private:
 	uint16_t m_width, m_height;
 
 	uint32_t m_tex_id;
+
+	int      m_format;
 
 	uint16_t m_ori_w, m_ori_h;
 //		     m_ori_x, m_ori_y;
--- painting2/source/Texture.cpp
+++ painting2/source/Texture.cpp
@@ -3,19 +3,20 @@
 namespace pt2
 {
 
-Texture::Texture(uint16_t w, uint16_t h, uint32_t id) 
+Texture::Texture(uint16_t w, uint16_t h, uint32_t id, int format)
 	: m_ori_w(0)
 	, m_ori_h(0)
 	, m_load_finished(true)
 {
-	Init(w, h, id);
+	Init(w, h, id, format);
 }
 
-void Texture::Init(uint16_t w, uint16_t h, uint32_t id) 
+void Texture::Init(uint16_t w, uint16_t h, uint32_t id, int format)
 {
-	m_width = w;
+	m_width  = w;
 	m_height = h;
 	m_tex_id = id;
+	m_format = format;
 }
 
 void Texture::InitOri(uint16_t ori_w, uint16_t ori_h)
